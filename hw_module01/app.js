6da2b1c037423444cd388436351b468f52ca4b3f
Olti Rugova <olti.rugova@student.uni-pr.edu>
2026-04-13 18:54:40 +0200

    Assignment01

---
+++ hw_module01/app.js
@@ -0,0 +1,12 @@
+const Person = require('./person.js');
+
+const P1 = new Person('Joni', 'QS');
+P1.getInfo();
+
+const P2 = new Person('Olti', 'QS');
+P2.getInfo();
+
+const P3 = new Person('Dreni', 'QS');
+P3.getInfo();
+
+P3.getCount();
